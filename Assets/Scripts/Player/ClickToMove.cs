cf6b416391a65881939fb4b5fb4dec4da695af43
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2023-02-07 11:33:13 +0000

    Player moves on click mouse
    Player movement now depends on where the player clicks

--- Assets/Scripts/Player/ClickToMove.cs
+++ Assets/Scripts/Player/ClickToMove.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClickToMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private InputAction mouseClickAction;
+    public float playerSpeed = 10;
+    public float rotationSpeed = 3;
+
+    private Camera mainCamera;
+    private Coroutine coroutine;
+    private Vector3 targetPosition;
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        //Detect pressed mouse
+        mouseClickAction.Enable();
+        //Use this so that when not used we can unsubscribe for this easily
+        mouseClickAction.performed += Move;
     }
+
+    private void OnDisable()
+    {
+        mouseClickAction.performed -= Move;
+        mouseClickAction.Disable();
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        //Where do we need to move towards.
+        //Converting screen coordinates into game coordinates.
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if(Physics.Raycast(ray: ray, hitInfo: out RaycastHit hit) && hit.collider)
+        {
+            //Checks if there is a current coroutine running, if there is one this will not run until
+            //Previous coroutine is finished
+            if (coroutine != null) StopCoroutine(coroutine);
+            coroutine = StartCoroutine(PlayerMoveTowards(hit.point));
+
+            //Gizmos
+            targetPosition = hit.point;
+        }
+    }
+
+    private IEnumerator PlayerMoveTowards(Vector3 target)
+    {
+        //Makes the target be equal to eye level
+        float playerDistanceToFloor = transform.position.y - target.y;
+        target.y += playerDistanceToFloor;
+        while(Vector3.Distance(transform.position, target) > 1.5f)
+        {
+
+            Vector3 destination = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
+            //transform.position = destination;
+
+            Vector3 direction = target - transform.position;
+            Vector3 movement = direction.normalized * playerSpeed * Time.deltaTime;
+
+
+            rb.velocity = direction.normalized * playerSpeed;
+
+
+
+            //Rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized), rotationSpeed * Time.deltaTime);
+
+            //To not make the while loop go crazy
+            yield return null;
+
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(targetPosition, 1);
+    }
+
+
 }
